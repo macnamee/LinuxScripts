f3959f8b9402895adf106f7e444cd8a7a3700653
macnamee <34869041+macnamee@users.noreply.github.com>
2019-04-26 10:07:18 +1000

    Update NewLinuxSetup.sh

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -3,60 +3,48 @@
 sudo apt update
 sudo apt upgrade -y
 
-echo ""
-echo "======= END ======="
+/bin/echo -e "\e[1;36m ""
+/bin/echo -e "\e[1;36m "======= END ======="
 read -p "Press Enter to continue [OpenSSH] next."
 
 
-echo ""
-echo ""
-echo "------- Start -------"
-sudo apt install openssh-server -y
-echo "======= END ======="
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo "------- Start -------"
+/bin/echo -e "\e[1;36m sudo apt install openssh-server -y
+/bin/echo -e "\e[1;36m echo "======= END ======="
 read -p "Press Enter to continue [NetTools, VIM, HTOP, NFS-Common] next."
 
 
-echo ""
-echo ""
-echo "------- Start -------"
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo "------- Start -------"
 sudo apt install net-tools -y
 sudo apt install vim -y
 sudo apt install htop -y
 sudo apt install nfs-common -y
 hostname -I
-echo "======= END ======="
+/bin/echo -e "\e[1;36m echo "======= END ======="
 read -p "Press Enter to continue [Create root account] next."
 
 
-echo ""
-echo ""
-echo "------- Start -------"
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo "------- Start -------"
 sudo passwd root
 sudo passwd -u root
-echo "======= END ======="
+/bin/echo -e "\e[1;36m echo "======= END ======="
 read -p "Press Enter to continue [Create Mount Points] nexy."
 
 
-echo ""
-echo ""
-echo "------- Start -------"
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo ""
+/bin/echo -e "\e[1;36m echo "------- Start -------"
 sudo mkdir -p /mnt/Install
 sudo mkdir -p /mnt/W
 sudo mkdir -p /mnt/Q
 sudo mkdir -p /mnt/P
 ls -la /mnt
-echo "======= END ======="
-
-
-
-
-
-
-
-
-
-
-
-
+/bin/echo -e "\e[1;36m echo "======= END ======="
 
 
