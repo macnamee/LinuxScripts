513cc957358e0e84743a45e560a35a2cf04216f0
passwdblog <57241364+passwdblog@users.noreply.github.com>
2019-12-28 13:40:02 +1100

    Update NewLinuxSetup.sh

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -5,18 +5,14 @@ sudo apt upgrade -y
 
 /bin/echo -e "\e[1;36m "
 /bin/echo -e "\e[1;36m ======= END ======="
+
 read -p "Press Enter to continue [OpenSSH] next."
-
-
-/bin/echo -e "\e[1;36m echo "
 /bin/echo -e "\e[1;36m echo "
 /bin/echo -e "\e[1;36m echo ------- Start -------"
 sudo apt install openssh-server -y
 /bin/echo -e "\e[1;36m echo ======= END ======="
+
 read -p "Press Enter to continue [NetTools, VIM, HTOP, NFS-Common] next."
-
-
-/bin/echo -e "\e[1;36m echo "
 /bin/echo -e "\e[1;36m echo "
 /bin/echo -e "\e[1;36m echo ------- Start -------"
 sudo apt install net-tools -y
@@ -25,26 +21,27 @@ sudo apt install htop -y
 sudo apt install nfs-common -y
 hostname -I
 /bin/echo -e "\e[1;36m echo ======= END ======="
+
+
 read -p "Press Enter to continue [Create root account] next."
-
-
-/bin/echo -e "\e[1;36m echo "
 /bin/echo -e "\e[1;36m echo "
 /bin/echo -e "\e[1;36m echo ------- Start -------"
 sudo passwd root
 sudo passwd -u root
 /bin/echo -e "\e[1;36m echo ======= END ======="
-read -p "Press Enter to continue [Create Mount Points] nexy."
 
+read -p "Press Enter to add HS and TR to the hosts file"
+echo '192.168.1.20    tr' | sudo tee -a /etc/hosts
+echo '192.168.1.90    hs' | sudo tee -a /etc/hosts
 
-/bin/echo -e "\e[1;36m echo "
-/bin/echo -e "\e[1;36m echo "
+read -p "Press Enter to creat /mnt/V and mount it"
 /bin/echo -e "\e[1;36m echo ------- Start -------"
-sudo mkdir -p /mnt/Install
-sudo mkdir -p /mnt/W
-sudo mkdir -p /mnt/Q
-sudo mkdir -p /mnt/P
+sudo mkdir -p /mnt/V
+echo 'hs:/WL_Curated  /mnt/V   nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+sudo mount -a
 ls -la /mnt
+/bin/echo -e "\e[1;36m echo "
+
 /bin/echo -e "\e[1;36m echo ======= END ======="
 
 
