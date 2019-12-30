ebbbecd418a896c8e15be96eb40899488262c764
macnamee <34869041+macnamee@users.noreply.github.com>
2019-12-30 15:03:14 +1100

    Update NewLinuxSetup.sh

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -1,44 +1,77 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
 
+read -p "Do apt update && apt upgrade [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    sudo apt update
+    sudo apt upgrade -y
+else
+    echo "Not doing update && upgrade this time."
+fi
 /bin/echo -e "\e[1;36m "
 /bin/echo -e "\e[1;36m ======= END ======="
 
-read -p "Press Enter to continue [OpenSSH] next."
-/bin/echo -e "\e[1;36m echo ------- Start -------"
-sudo apt install openssh-server -y
-/bin/echo -e "\e[1;36m echo ======= END ======="
 
-read -p "Press Enter to continue [NetTools, VIM, HTOP, NFS-Common] next."
-/bin/echo -e "\e[1;36m echo ------- Start -------"
-sudo apt install net-tools -y
-sudo apt install vim -y
-sudo apt install htop -y
-sudo apt install nfs-common -y
-echo "alias ll='ls -la'" | sudo tee -a ~/.bashrc
+read -p "Install OpenSSH Sever [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    sudo apt install openssh-server -y
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not installing OpenSSH Server."
+fi
 
-read -p "Press Enter to continue [Create root account] next."
-/bin/echo -e "\e[1;36m echo ------- Start -------"
-sudo passwd root
-sudo passwd -u root
-/bin/echo -e "\e[1;36m echo ======= END ======="
+read -p "Install common tools: NetTools, VIM, HTOP and NFS-Common [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    sudo apt install net-tools -y
+    sudo apt install vim -y
+    sudo apt install htop -y
+    sudo apt install nfs-common -y
+    echo "alias ll='ls -la'" | sudo tee -a ~/.bashrc
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not installing common tools."
+fi
 
-read -p "Press Enter to add HS and TR to hostst."
-/bin/echo -e "\e[1;36m echo ------- Start -------"
-echo '192.168.1.20    tr' | sudo tee -a /etc/hosts
-echo '192.168.1.90    hs' | sudo tee -a /etc/hosts
-/bin/echo -e "\e[1;36m echo ======= END ======="
+
+read -p "Enable and set a password for root? [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    sudo passwd root
+    sudo passwd -u root
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not setting up a root account."
+fi
+
+read -p "Add HS and TS to /etc/hosts? [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    echo '192.168.1.20    tr' | sudo tee -a /etc/hosts
+    echo '192.168.1.90    hs' | sudo tee -a /etc/hosts
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not updating /etc/hosts."
+fi
 
 
-read -p "Press Enter to create /mnt/V and update fstab."
-/bin/echo -e "\e[1;36m echo ------- Start -------"
-sudo mkdir -p /mnt/V
-echo 'hs:/WL_Curated  /mnt/V   nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
-sudo mount -a
-ls -la /mnt
-/bin/echo -e "\e[1;36m echo ======= END ======="
-
+read -p "mount HS/V on /mnt/V? (Needs HS in the /etc/hosts file) [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    sudo mkdir -p /mnt/V
+    echo 'hs:/WL_Curated  /mnt/V   nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    sudo mount -a
+    ls -la /mnt
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not mounting /mnt/V"
+fi
 
 hostname -I
-/bin/echo -e "\e[1;36m echo ======= END ======="
+/bin/echo -e "\e[1;36m echo ======= DONE ======="
