b1f5061b90a5fa6a9feb035074ce50113d5595b3
macnamee <34869041+macnamee@users.noreply.github.com>
2020-03-15 19:40:44 +1100

    Update NewLinuxSetup.sh

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -33,6 +33,7 @@ if [[ $REPLY = y ]] ; then
     sudo apt install htop -y
     sudo apt install nfs-common -y
     sudo apt install tilix -y
+    sudo apt install glances -y
     echo "alias ll='ls -la'" | sudo tee -a ~/.bashrc
     echo "alias cls='clear'" | sudo tee -a ~/.bashrc
     /bin/echo -e "\e[1;36m echo ======= END ======="
