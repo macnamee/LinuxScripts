acd52f636880c50587f90d24133dd387a456e6d2
macnamee <34869041+macnamee@users.noreply.github.com>
2020-05-18 19:02:47 +1000

    Update get_plots.sh

--- get_plots.sh
+++ get_plots.sh
@@ -10,7 +10,6 @@ do
     cat ttt.txt  | sed "s/^/$server/"
 done
 
-
 server=chia01
 drives=(4TB_01 4TB_02 4TB_03 4TB_04 4TB_05 4TB_06 4TB_07 4TB_08 4TB_09 4TB_10 4TB_11 4TB_12 4TB_13 3TB_01 3TB_02 3TB_03)
 for i in "${drives[@]}"
@@ -27,7 +26,6 @@ do
     cat ttt.txt  | sed "s/^/$server/"
 done
 
-
 server=chia04
 drives=(1000_01GB 1000_02GB 1000_03GB 2000_01GB 2000_02GB 4000_01GB 4000_02GB 4000_03GB)
 for i in "${drives[@]}"
@@ -36,7 +34,6 @@ do
     cat ttt.txt  | sed "s/^/$server/"
 done
 
-
 server=chia05
 drives=(600GB_01 600GB_02 600GB_03 600GB_04 600GB_05 600GB_06 600GB_07 600GB_08 600GB_09 600GB_10 600GB_11 600GB_12)
 for i in "${drives[@]}"
@@ -44,7 +41,6 @@ do
     ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
     cat ttt.txt  | sed "s/^/$server/"
 done
-
 
 server=lxcfarmer
 drives=(zz_10TB_01 zz_10TB_02 zz_10TB_03 zz_10TB_04 zz_10TB_05 zz_10TB_06)
