1cf4d9e8d25e89e6625174af24fbd232836ca73a
macnamee <34869041+macnamee@users.noreply.github.com>
2020-05-18 19:00:26 +1000

    Change to array

--- get_plots.sh
+++ get_plots.sh
@@ -1,156 +1,55 @@
 #!/bin/bash
-
 rm ttt.txt
-
 echo "Server/mnt/Drive/Folder/FileName"
+
 server=chia00
-ssh $server "ls /mnt/4TB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_04/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_05/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_06/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_07/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_08/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
+drives=(4TB_01 4TB_02 4TB_03 4TB_04 4TB_05 4TB_06 4TB_07 4TB_08)
+for i in "${drives[@]}"
+do
+    ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
+    cat ttt.txt  | sed "s/^/$server/"
+done
 
 
 server=chia01
-ssh $server "ls /mnt/4TB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_04/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_05/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_06/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_07/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_08/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_09/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_10/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_11/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_12/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4TB_13/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/3TB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/3TB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/3TB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-
+drives=(4TB_01 4TB_02 4TB_03 4TB_04 4TB_05 4TB_06 4TB_07 4TB_08 4TB_09 4TB_10 4TB_11 4TB_12 4TB_13 3TB_01 3TB_02 3TB_03)
+for i in "${drives[@]}"
+do
+    ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
+    cat ttt.txt  | sed "s/^/$server/"
+done
 
 server=chia03
-ssh $server "ls /mnt/3000GB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/3000GB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/2000GB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/2000GB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/2000GB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/2000GB_04/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_04/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_05/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_06/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_07/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_08/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_09/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_10/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
+drives=(3000GB_01 3000GB_02 2000GB_01 2000GB_02 2000GB_03 2000GB_04 600GB_01 600GB_02 600GB_03 600GB_04 600GB_05 600GB_06 600GB_07 600GB_08 600GB_09 600GB_10)
+for i in "${drives[@]}"
+do
+    ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
+    cat ttt.txt  | sed "s/^/$server/"
+done
 
 
 server=chia04
-ssh $server "ls /mnt/1000_01GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/1000_02GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/1000_03GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/2000_01GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/2000_02GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4000_01GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4000_02GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/4000_03GB/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-
+drives=(1000_01GB 1000_02GB 1000_03GB 2000_01GB 2000_02GB 4000_01GB 4000_02GB 4000_03GB)
+for i in "${drives[@]}"
+do
+    ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
+    cat ttt.txt  | sed "s/^/$server/"
+done
 
 
 server=chia05
-ssh $server "ls /mnt/600GB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_04/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_05/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_06/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_07/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_08/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_09/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_10/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_11/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/600GB_12/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-
-
+drives=(600GB_01 600GB_02 600GB_03 600GB_04 600GB_05 600GB_06 600GB_07 600GB_08 600GB_09 600GB_10 600GB_11 600GB_12)
+for i in "${drives[@]}"
+do
+    ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
+    cat ttt.txt  | sed "s/^/$server/"
+done
 
 
 server=lxcfarmer
-ssh $server "ls /mnt/zz_10TB_01/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/zz_10TB_02/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/zz_10TB_03/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/zz_10TB_04/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/zz_10TB_05/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
-ssh $server "ls /mnt/zz_10TB_06/Plots/*.dat" > ttt.txt
-cat ttt.txt  | sed "s/^/$server/"
+drives=(zz_10TB_01 zz_10TB_02 zz_10TB_03 zz_10TB_04 zz_10TB_05 zz_10TB_06)
+for i in "${drives[@]}"
+do
+    ssh $server "ls /mnt/$i/Plots/*.dat" > ttt.txt
+    cat ttt.txt  | sed "s/^/$server/"
+done
