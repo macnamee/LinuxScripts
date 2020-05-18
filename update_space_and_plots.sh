4741e730b3805e76da42c870a2e5dc81c7892733
macnamee <34869041+macnamee@users.noreply.github.com>
2020-05-18 18:42:41 +1000

    Update update_space_and_plots.sh

--- update_space_and_plots.sh
+++ update_space_and_plots.sh
@@ -10,17 +10,17 @@ wget https://raw.githubusercontent.com/macnamee/LinuxScripts/master/get_space.sh
 chmod +x get_plots.sh
 chmod +x get_space.sh
 
-/bin/echo -e "\e[1;96m======================================================================\e[1;96m"
-/bin/echo -e "\e[1;96m   Getting space info. V1.01\e[1;96m"
-/bin/echo -e "\e[1;96m======================================================================\e[1;96m"
+/bin/echo -e "\e[1;96m======================================================================\e[1;39m"
+/bin/echo -e "\e[1;96m   Getting space info. V1.01\e[1;36m"
+/bin/echo -e "\e[1;96m======================================================================\e[1;36m"
 ./get_space.sh > /mnt/P/space.txt
 
 
 
-/bin/echo -e "\e[1;36m "
-/bin/echo -e "\e[1;36m "
+/bin/echo -e "\e[1;39m"
+/bin/echo -e "\e[1;39m "
 /bin/echo -e "\e[1;96m======================================================================\e[1;39m"
-/bin/echo -e "\e[1;96mGetting plot info.\e[1;96m"
+/bin/echo -e "\e[1;96mGetting plot info.\e[1;39m"
 /bin/echo -e "\e[1;96m======================================================================\e[1;39m"
 ./get_plots.sh > /mnt/P/plots.txt
 
