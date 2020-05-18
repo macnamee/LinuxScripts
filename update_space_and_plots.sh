931d1ede1d9e0e365d2bb85b15294f607f47708d
macnamee <34869041+macnamee@users.noreply.github.com>
2020-05-18 14:24:03 +1000

    Update update_space_and_plots.sh

--- update_space_and_plots.sh
+++ update_space_and_plots.sh
@@ -4,11 +4,11 @@ cd ~
 rm get_plots.sh
 rm get_space.sh
 
-https://raw.githubusercontent.com/macnamee/LinuxScripts/master/get_plots.sh
-https://raw.githubusercontent.com/macnamee/LinuxScripts/master/get_space.sh
+wget https://raw.githubusercontent.com/macnamee/LinuxScripts/master/get_plots.sh
+wget https://raw.githubusercontent.com/macnamee/LinuxScripts/master/get_space.sh
 
 /bin/echo -e "\e[1;92m======================================================================\e[1;39m"
-/bin/echo -e "\e[1;92m   Getting space info.\e[1;39m"
+/bin/echo -e "\e[1;92m   Getting space info. V1.00\e[1;39m"
 /bin/echo -e "\e[1;92m======================================================================\e[1;39m"
 #./get_space.sh > /mnt/P/space.txt
 
