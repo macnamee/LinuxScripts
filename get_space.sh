09b94167fa7fe70e496d320d85d6cc043730c11f
macnamee <34869041+macnamee@users.noreply.github.com>
2020-05-18 14:43:32 +1000

    Added chia05

--- get_space.sh
+++ get_space.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Server Drive Total Used Avilable Percentage DriveName NrOfFiles"
-server=chia-farmer
+server=chia00
 space=$(ssh $server "df -k /mnt/4TB_01" | tail -n 1)
 files=$(ssh $server "ls /mnt/4TB_01/Plots/*.dat" | wc -l)
 echo $server $space $files
@@ -157,7 +157,43 @@ files=$(ssh $server "ls /mnt/4000_03GB/Plots/*.dat" | wc -l)
 echo $server $space $files
 
 
-
+server=chia05
+space=$(ssh $server "df -k /mnt/600_GB_01" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_01/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_02" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_02/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_03" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_03/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_04" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_04/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_05" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_05/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_06" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_06/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_07" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_07/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_08" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_08/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_09" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_09/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_10" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_10/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_11" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_11/Plots/*.dat" | wc -l)
+echo $server $space $files
+space=$(ssh $server "df -k /mnt/600_GB_12" | tail -n 1)
+files=$(ssh $server "ls /mnt/600GB_12/Plots/*.dat" | wc -l)
+echo $server $space $files
 
 server=lxcfarmer
 space=$(ssh $server "df -k /mnt/zz_10TB_01" | tail -n 1)
