adf64aa926e7537cff681dcc00a68940e0756037
macnamee <34869041+macnamee@users.noreply.github.com>
2020-07-14 18:51:00 +1000

    Update send_task_complete_email.sh

--- send_task_complete_email.sh
+++ send_task_complete_email.sh
@@ -5,4 +5,4 @@ echo 'To:macnamee@gmail.com' | tee ~/msg.txt
 echo 'From:Daemon@macnamee.org' | tee -a ~/msg.txt
 echo 'Subject:'$HOSTNAME 'Task Complete' | tee -a ~/msg.txt
 
-ssmtp SomeName@gmail.com < msg.txt
+ssmtp macnamee@gmail.com < msg.txt
