650d668c291f1c30989db22584cd043f82cc062f
macnamee <34869041+macnamee@users.noreply.github.com>
2020-08-09 14:31:01 +1000

    Splot mounting options between the three servers with Y/N options

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -36,6 +36,7 @@ if [[ $REPLY = y ]] ; then
     sudo apt install glances -y
     sudo apt install ssmtp -y
     sudo apt install neofetch -y
+    sudo apt install zim -y
     sudo add-apt-repository ppa:bashtop-monitor/bashtop && sudo apt update && sudo apt install bashtop -y
     echo "alias ll='ls -lah'" | sudo tee -a ~/.bashrc
     echo "alias cls='clear'" | sudo tee -a ~/.bashrc
@@ -97,8 +98,7 @@ else
     echo "Not updating /etc/hosts."
 fi
 
-
-read -p "mount HS/V on /mnt/V? (Needs HS in the /etc/hosts file) [y/n]" -n 1 -r
+read -p "Mount PQVW and zz Drives from HS on /mnt? [y/n]" -n 1 -r
 echo
 if [[ $REPLY = y ]] ; then
     /bin/echo -e "\e[1;36m echo ------- Start -------"
@@ -106,20 +106,62 @@ if [[ $REPLY = y ]] ; then
     sudo mkdir -p /mnt/Q
     sudo mkdir -p /mnt/V
     sudo mkdir -p /mnt/W
-    sudo mkdir -p /mnt/HS2Temp
+    sudo mkdir -p /mnt/zz_10TB_01
+    sudo mkdir -p /mnt/zz_10TB_02
+    sudo mkdir -p /mnt/zz_10TB_03
+    sudo mkdir -p /mnt/zz_10TB_04
+    sudo mkdir -p /mnt/zz_10TB_05
+    sudo mkdir -p /mnt/zz_10TB_06
+    
     echo 'hs:/2xNVME        /mnt/P             nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
     echo 'hs:/1TBSSD        /mnt/Q             nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
     echo 'hs:/WL_Curated    /mnt/V             nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
     echo 'hs:/Wordlist      /mnt/W             nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs:/zz_10TB_01    /mnt/zz_10TB_01    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs:/zz_10TB_02    /mnt/zz_10TB_02    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs:/zz_10TB_03    /mnt/zz_10TB_03    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs:/zz_10TB_04    /mnt/zz_10TB_04    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs:/zz_10TB_05    /mnt/zz_10TB_05    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs:/zz_10TB_06    /mnt/zz_10TB_06    nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
+
+    sudo mount -a
+    cd /mnt && sudo chmod 777 * && sudo chown ryan * && sudo chgrp ryan *
+    ls -la /mnt
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not mounting PQVW and zz Drives from HS"
+fi
+
+
+read -p "Mount HS2/Temp on /mnt? [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    sudo mkdir -p /mnt/HS2Temp
     echo 'hs2:/Temp         /mnt/HS2Temp       nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    sudo mount -a
+    cd /mnt && sudo chmod 777 * && sudo chown ryan * && sudo chgrp ryan *
+    ls -la /mnt
+    /bin/echo -e "\e[1;36m echo ======= END ======="
+else
+    echo "Not mounting HS2Temp"
+fi
+
+
+read -p "Mount TR 4TB on /mnt? [y/n]" -n 1 -r
+echo
+if [[ $REPLY = y ]] ; then
+    /bin/echo -e "\e[1;36m echo ------- Start -------"
+    sudo mkdir -p /mnt/TR_4TB_RAID0
     echo 'tr:/mnt/4TB_RAID0 /mnt/TR_4TB_RAID0  nfs auto,noatime,nolock,bg,nfsvers=3,intr,tcp,actimeo=1800 0 0' | sudo tee -a /etc/fstab
     sudo mount -a
     cd /mnt && sudo chmod 777 * && sudo chown ryan * && sudo chgrp ryan *
     ls -la /mnt
     /bin/echo -e "\e[1;36m echo ======= END ======="
 else
-    echo "Not mounting /mnt/V"
+    echo "Not mounting TR"
 fi
+
 
 hostname -I
 /bin/echo -e "\e[1;36m echo ======= DONE ======="
