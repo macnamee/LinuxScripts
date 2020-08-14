4e26776c2fd695c959b0fadaa3436e5db45e180a
ryan <ryan@MAXX-MATE>
2020-08-14 16:03:58 +1000

    Updated P1

--- P1.sh
+++ P1.sh
@@ -4,7 +4,7 @@ source ./chia-blockchain/activate
 
 cd chia-blockchain
 
-chia-create-plots -k 31 -i 46520 -n 10 -t /mnt/512GBNVME/Plots -d /mnt/zz_10TB_06/Plots/
+chia-create-plots -k 31 -i 4000 -n 10 -t /mnt/512GBNVME/T_Plots -2 /mnt/512GBNVME/T_Plots -d /mnt/zz_10TB_06/Plots/
 
 
 ~/send_task_complete_email.sh
