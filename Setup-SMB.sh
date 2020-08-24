292871fbdb6f12792430c5584ae54df7841d3311
macnamee <34869041+macnamee@users.noreply.github.com>
2020-08-24 20:25:32 +1000

    changed to tee the whole config file

--- Setup-SMB.sh
+++ Setup-SMB.sh
@@ -2,14 +2,26 @@
 
 sudo apt install tasksel 
 sudo tasksel install samba-server 
-
-sudo cp /etc/samba/smb.conf /etc/samba/smb.conf_backup  
-sudo bash -c 'grep -v -E "^#|^;" /etc/samba/smb.conf_backup | grep . > /etc/samba/smb.conf' 
-
 sudo smbpasswd -a ryan 
-
 sudo mkdir /var/temp 
 sudo chmod 777 /var/temp/ 
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf_backup
+
+echo "[global]"                                           | sudo tee -a /etc/samba/smb.conf
+echo "   workgroup = WORKGROUP"                           | sudo tee -a /etc/samba/smb.conf
+echo "   server string = %h server (Samba, Ubuntu)"       | sudo tee -a /etc/samba/smb.conf
+echo "   log file = /var/log/samba/log.%m"                | sudo tee -a /etc/samba/smb.conf
+echo "   max log size = 1000"                             | sudo tee -a /etc/samba/smb.conf
+echo "   logging = file"                                  | sudo tee -a /etc/samba/smb.conf
+echo "   panic action = /usr/share/samba/panic-action %d" | sudo tee -a /etc/samba/smb.conf
+echo "   server role = standalone server"                 | sudo tee -a /etc/samba/smb.conf
+echo "   obey pam restrictions = yes"                     | sudo tee -a /etc/samba/smb.conf
+echo "   unix password sync = yes"                        | sudo tee -a /etc/samba/smb.conf
+echo "   passwd program = /usr/bin/passwd %u"             | sudo tee -a /etc/samba/smb.conf
+echo "   passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* ." | sudo tee -a /etc/samba/smb.conf
+echo "   pam password change = yes"                       | sudo tee -a /etc/samba/smb.conf
+echo "   map to guest = bad user"                         | sudo tee -a /etc/samba/smb.conf
+echo "   usershare allow guests = yes"                    | sudo tee -a /etc/samba/smb.conf
 
 echo "[homes]"                              | sudo tee -a /etc/samba/smb.conf
 echo "   comment = Home Directories"        | sudo tee -a /etc/samba/smb.conf
@@ -29,4 +41,3 @@ echo "   writable = yes"                    | sudo tee -a /etc/samba/smb.conf
 echo "   guest ok = yes"                    | sudo tee -a /etc/samba/smb.conf
 
 sudo systemctl restart smbd 
-
