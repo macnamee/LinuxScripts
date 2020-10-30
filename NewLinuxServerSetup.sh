eecb00d6264a2661bd56b12f548180b1174629bb
macnamee <34869041+macnamee@users.noreply.github.com>
2020-10-30 13:05:07 +1100

    Commented out bashtop

--- NewLinuxServerSetup.sh
+++ NewLinuxServerSetup.sh
@@ -38,7 +38,7 @@ if [[ $REPLY = y ]] ; then
     sudo apt install iperf3 -y
     sudo apt install hardinfo -y
 
-    sudo add-apt-repository ppa:bashtop-monitor/bashtop && sudo apt update && sudo apt install bashtop -y
+    #sudo add-apt-repository ppa:bashtop-monitor/bashtop && sudo apt update && sudo apt install bashtop -y
     echo "alias ll='ls -lah'" | sudo tee -a ~/.bashrc
     echo "alias cls='clear'" | sudo tee -a ~/.bashrc
     echo "neofetch" | sudo tee -a ~/.bashrc
