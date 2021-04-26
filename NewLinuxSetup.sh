9d8667374c91a9bfd266f609d0c0239e8416ee20
macnamee <34869041+macnamee@users.noreply.github.com>
2021-04-26 13:12:12 +1000

    fixing HS2 HS3 PRO mounts

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -70,6 +70,10 @@ echo
 if [[ $REPLY = y ]] ; then
     /bin/echo -e "\e[1;36m echo ------- Start -------"
     echo '192.168.1.90    hs' | sudo tee -a /etc/hosts
+    echo '192.168.1.22    hs2' | sudo tee -a /etc/hosts
+    echo '192.168.1.37    pro' | sudo tee -a /etc/hosts
+    echo '192.168.1.23    hs3' | sudo tee -a /etc/hosts
+    echo '192.168.1.246   fullnode' | sudo tee -a /etc/hosts
     /bin/echo -e "\e[1;36m echo ======= END ======="
 else
     echo "Not updating /etc/hosts."
@@ -80,10 +84,14 @@ echo
 if [[ $REPLY = y ]] ; then
     /bin/echo -e "\e[1;36m echo ------- Start -------"
     sudo mkdir -p /mnt/chia
-    sudo mkdir -p /mnt/HS_4TB
+    sudo mkdir -p /mnt/HS2_Temp
+    sudo mkdir -p /mnt/HS3_Temp
+    sudo mkdir -p /mnt/PRO_Temp    
     
-    echo 'hs:/chia         /mnt/chia         nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
-    echo 'hs:/Temp         /mnt/HS_4TB       nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
+    echo 'hs:/chia           /mnt/chia         nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
+    echo 'hs2:/Temp          /mnt/HS2_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
+    echo 'hs3:/Temp          /mnt/HS3_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
+    echo 'pro:/mnt/4TB_RAID0 /mnt/PRO_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab        
     sudo mount -a
     cd /mnt && sudo chmod 777 * && sudo chown ryan * && sudo chgrp ryan *
     ls -la /mnt
