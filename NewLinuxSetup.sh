48b31f14f9f59f5c4f95aa85a32b4584fa8f85fd
macnamee <34869041+macnamee@users.noreply.github.com>
2021-08-02 20:32:07 +1000

    Added .inputrc

--- NewLinuxSetup.sh
+++ NewLinuxSetup.sh
@@ -48,6 +48,12 @@ if [[ $REPLY = y ]] ; then
     echo "neofetch" | sudo tee -a ~/.bashrc
     echo 'root:macnamee2@gmail.com:smtp.gmail.com:587' | sudo tee -a /etc/ssmtp/revaliases
     echo 'ryan:macnamee2@gmail.com:smtp.gmail.com:587' | sudo tee -a /etc/ssmtp/revaliases
+    
+    echo '"\e[A": history-search-backward' | sudo tee -a .inputrc
+    echo '"\e[B": history-search-forward' | sudo tee -a .inputrc
+    echo '"\e[C": forward-char' | sudo tee -a .inputrc
+    echo '"\e[D": backward-char' | sudo tee -a .inputrc
+
     /bin/echo -e "\e[1;36m echo ======= END ======="
 else
     echo "Not installing common tools."
@@ -70,10 +76,6 @@ echo
 if [[ $REPLY = y ]] ; then
     /bin/echo -e "\e[1;36m echo ------- Start -------"
     echo '192.168.1.90    hs' | sudo tee -a /etc/hosts
-    #echo '192.168.1.22    hs2' | sudo tee -a /etc/hosts
-    #echo '192.168.1.37    pro' | sudo tee -a /etc/hosts
-    #echo '192.168.1.23    hs3' | sudo tee -a /etc/hosts
-    echo '192.168.1.114   i5'  | sudo tee -a /etc/hosts
     echo '192.168.1.246   fullnode' | sudo tee -a /etc/hosts
     /bin/echo -e "\e[1;36m echo ======= END ======="
 else
@@ -85,16 +87,8 @@ echo
 if [[ $REPLY = y ]] ; then
     /bin/echo -e "\e[1;36m echo ------- Start -------"
     sudo mkdir -p /mnt/chia
-    #sudo mkdir -p /mnt/HS2_Temp
-    #sudo mkdir -p /mnt/HS3_Temp
-    #sudo mkdir -p /mnt/PRO_Temp    
-    sudo mkdir -p /mnt/i5_Temp        
-    
-    echo 'hs:/chia           /mnt/chia         nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
-    #echo 'hs2:/Temp          /mnt/HS2_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
-    #echo 'hs3:/Temp          /mnt/HS3_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
-    #echo 'pro:/mnt/4TB_RAID0 /mnt/PRO_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
-    echo 'i5:/mnt/Temp          /mnt/i5_Temp     nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab    
+        
+    echo 'hs:/chia            /chia         nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1' | sudo tee -a /etc/fstab
     sudo mount -a
     cd /mnt && sudo chmod 777 * && sudo chown ryan * && sudo chgrp ryan *
     ls -la /mnt
