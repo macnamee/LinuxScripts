918a9414398995a4dbc163d10b2e9c8e51c22439
macnamee <34869041+macnamee@users.noreply.github.com>
2021-10-17 23:00:04 +1100

    Initial Setup

---
+++ Setup-Dev.sh
@@ -0,0 +1,18 @@
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+
+
+echo eval "$(oh-my-posh --init --shell bash --config ~/jandedobbeleer.omp.json)" | sudo tee -a /home/ryan/.bashrc
+
+
+
+cd ~
+cp jandedobbeleer.omp.json ~
+
+. ~/.bashrc
